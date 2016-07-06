8d813709c9da1c568c2d8eedd6638dfe6993b748
Onni Lampi <contact@onnilampi.fi>
2016-07-06 13:44:59 +0300

    setuppia ja READMEtä parannettu

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Helsingin Kotkien toimintakertomus 2015-2016
 
-Suhteellisen simppeli setti, juuressa Makefile ja kattofilu, kuvat ja osiot omissa kansioissaan.
+Suhteellisen simppeli setti, juuressa Makefile ja kattofilu, kuvat ja osiot omissa kansioissaan. Skripti `setup.sh` lataa tarvittavat paketit ja latoo dokumentit. Skripti toimii Debianilla ja sen johdannaisilla (Ubuntu, Mint...).
 
 ### Tehnyt
 
--- setup.sh
+++ setup.sh
@@ -1,2 +1,35 @@
-apt-get install git make texlive texlive-extra-utils texlive-fonts*  texlive-lang-finnish  texlive-lang-english texlive-latex-extra
+#!/bin/bash
+
+clear
+
+[ $UID -eq 0 ] && echo "Skriptiä ei tule ajaa sudona, jotta ladottujen dokumenttien käyttöoikeudet pysyvät kunnossa. Skriptissä ajetaan pakettien asennus sudona erikseen." && exit 1
+
+echo "sudona ajetaan vain ja ainoastaan pakettien asentaminen"
+
+secs='5 4 3 2 1 0'
+
+for second in $secs;do
+	echo $second
+	sleep 1
+done
+
+sudo apt-get install texlive texlive-latex-recommended texlive-latex-extra texlive-lang-european make tree inotify-tools 
+
+clear
+
+echo "seuraavaksi ladotaan dokumentit"
+
+secs='5 4 3 2 1 0'
+
+for second in $secs;do
+	echo $second
+	sleep 1
+done
+
 make
+
+clear
+
+echo "Dokumentit ladottu. Symboliset linkit .pdf-tiedostoihin löytyvät kansiosta walpuri-public."
+echo "Muista lukea README.md!!!"
+tree -l
