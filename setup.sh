887aab69627d523690e64fb7415cb340c78ff980
Onni Lampi <contact@onnilampi.fi>
2016-07-13 22:24:32 +0300

    setup.sh päivitetty, nettiversio tokesta kunnossa

--- setup.sh
+++ setup.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
 
+varmistus(){
+	read -n1 -rsp $'Luettuasi ja ymmärrettyäsi yllä olevan tekstin, paina ANY-näppäintä.\nKeskeytys normaalisti CTRL+C:llä.\n'
+}
+
+
 clear
 
 [ $UID -eq 0 ] && echo "Skriptiä ei tule ajaa sudona, jotta ladottujen dokumenttien käyttöoikeudet pysyvät kunnossa. Skriptissä ajetaan pakettien asennus sudona erikseen." && exit 1
 
-echo "sudona ajetaan vain ja ainoastaan pakettien asentaminen"
-
-secs='5 4 3 2 1 0'
-
-for second in $secs;do
-	echo $second
-	sleep 1
-done
-
-sudo apt-get install texlive texlive-latex-recommended texlive-latex-extra texlive-lang-european make tree inotify-tools 
+echo "Sudona ajetaan vain ja ainoastaan pakettien asentaminen. Alla lyhyet selostukset siitä, mitä asennetaan ja miksi. Kaikki löytyvät ainakin Debian Jessien main-repositorystä."
+echo "Asennettavat paketit:"
+echo " * texlive, texlive-latex-recommended, texlive-latex-extra, texlive-lang-european - tarvittavat paketit, jotta dokumenttien latominen onnistuu"
+echo " * make - Makefilujen käyttäminen on kätevää ja tämä työkalu tarvitaan vain ja ainoastaan siksi"
+echo " * tree - Työkalu hakemistopolun tekemiseen"
+echo " * inotify-tools - Mahdollistaa makron, jonka avulla dokumentteja voidaan latoa automaattisesti"
+echo " * apache2-utils - paketin mukana tulee noin 10 web-serverin hallintaan tarkoitettua työkalua joista htpasswd on erityisen kätevä salasanojen hallinnassa."
+echo ""
+varmistus
 
 clear
 
-echo "seuraavaksi ladotaan dokumentit"
+sudo apt-get install texlive texlive-latex-recommended texlive-latex-extra texlive-lang-european make tree inotify-tools apache2-utils
+echo ""
+echo ""
+echo ""
+echo ""
 
-secs='5 4 3 2 1 0'
 
-for second in $secs;do
-	echo $second
-	sleep 1
-done
+echo "Paketit toivon mukaan asennettu oikein, seuraavaksi ladotaan dokumentit. Tässä saattaa kestää muutama minuutti, mikäli jokainen mahdollinen dokumentti ladotaan. Odota kärsivällisesti, niin kauan kuin latominen ei pysähdy, kaikki on OK. :)"
+echo ""
+
+varmistus
+
+clear
 
 make
 
 clear
 
+tree -l
 echo "Dokumentit ladottu. Symboliset linkit .pdf-tiedostoihin löytyvät kansiosta walpuri-public."
 echo "Muista lukea README.md!!!"
-tree -l
